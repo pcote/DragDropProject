4f6c5322ecf69f35213225159953cbe35b9d0192
Phil Cote <cotejrp@gmail.com>
2017-02-27 20:33:35 -0500

    Make it so files are listed in the order that they get uploaded in.

--- main.py
+++ main.py
@@ -18,15 +18,22 @@ def send_file():
     filename = secure_filename(fileob.filename)
     save_path = "{}/{}".format(app.config["UPLOAD_FOLDER"], filename)
     fileob.save(save_path)
+
+    # open and close to update the access time.
+    with open(save_path, "r") as f:
+        pass
+
     return "okay did something"
 
 
 @app.route("/filenames", methods=["GET"])
 def get_filenames():
     filenames = os.listdir("uploads/")
+    modify_time_sort = lambda f: os.stat("uploads/{}".format(f)).st_atime
+    filenames = sorted(filenames, key=modify_time_sort)
     return_dict = dict(filenames=filenames)
     return jsonify(return_dict)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
